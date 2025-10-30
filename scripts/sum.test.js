8116d4b3f2a344ec97760f5b9419fc6fe02c3365
sjose878 <sjose878@mtroyal.ca>
2025-10-30 17:50:02 -0600

    Added a new test case for sum

--- scripts/sum.test.js
+++ scripts/sum.test.js
@@ -5,3 +5,7 @@ const sum = require('./sum');
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+test('adds -1 to -1 to equal -2', () => {
+  expect(sum(-1, -1)).toBe(-2);
+})
