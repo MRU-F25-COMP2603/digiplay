cf2d62543ffb1236bd5b28f70c8f64da4d7f0e30
sjose878 <sjose878@mtroyal.ca>
2025-10-30 18:03:05 -0600

    Renamed sum accessor to calculator Added sub function Added a sub test case

--- scripts/sum.js
+++ scripts/sum.js
@@ -3,4 +3,10 @@
 function sum(a, b) {
   return a + b;
 }
-module.exports = sum;
+
+function sub(a, b) {
+  return a - b;
+}
+module.exports = {
+  sum
+};
--- scripts/sum.test.js
+++ scripts/sum.test.js
@@ -1,11 +1,11 @@
 // https://archive.jestjs.io/docs/en/22.x/getting-started
 
-const sum = require('./sum');
+const calculator = require('./sum');
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+  expect(calculator.sum(1, 2)).toBe(3);
 });
 
 test('adds -1 to -1 to equal -2', () => {
-  expect(sum(-1, -1)).toBe(-2);
+  expect(calculator.sum(-1, -1)).toBe(-2);
 })
